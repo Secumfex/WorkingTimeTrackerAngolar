5ceea9e27249836a44c7dff54b80b9c7691a93ea
Arend Buchacher <Arend.Buchacher@gmx.de>
2022-01-18 22:00:12 +0100

    ng generate component list-component

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ControlareaComponent } from './controlarea/controlarea.component';
+import { ListComponentComponent } from './list-component/list-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ControlareaComponent
+    ControlareaComponent,
+    ListComponentComponent
   ],
   imports: [
     BrowserModule,
