753e1f9dbc725ca0d50ee1f5467f22d8a46d5441
Arend Buchacher <Arend.Buchacher@gmx.de>
2022-01-19 22:53:40 +0100

    Add basic routing capabilities

--- my-first-project/src/app/app-routing.module.ts
+++ my-first-project/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponentComponent } from './list-component/list-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
