e3d4e1e154b33d46aa3c9c55d5fd9fc107e81e7a
Arend Buchacher <Arend.Buchacher@gmx.de>
2022-01-19 23:20:39 +0100

    Add som basic routing

--- my-first-project/src/app/app-routing.module.ts
+++ my-first-project/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListComponentComponent } from './list-component/list-component.component';
 
 const routes: Routes = [
-  { path: 'list', component: ListComponentComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // root will be redirected.
+  { path: 'list', component: ListComponentComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
