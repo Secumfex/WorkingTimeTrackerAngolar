c84622b7277967d2e24df54b36ab337173428704
Arend Buchacher <Arend.Buchacher@gmx.de>
2022-02-02 21:19:04 +0100

    Add stub data to calendar service
    - Yes, it's a shameless copy from the other stupid stub service

--- my-first-project/src/app/google-calendar.service.ts
+++ my-first-project/src/app/google-calendar.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { CalendarEvent } from './calendar-event';
+import { STUBEVENTS } from './stubevents';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleCalendarService {
+  getEvents(): Observable<CalendarEvent[]> {
+    const events = of(STUBEVENTS); // Still just a stub.
+    return events;
+  }
 
+  getEvent(title:string): Observable<CalendarEvent> {
+    // For now, assume that a data with the specified `id` always exists.
+    // This is why there is an ! at the end.
+    const event = STUBEVENTS.find( event => event.title == title )!;
+    return of(event);
+  }
   constructor() { }
 }
