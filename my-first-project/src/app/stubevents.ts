29d3764dd09ac60969365cc5cfda875a90ef055b
Arend Buchacher <Arend.Buchacher@gmx.de>
2022-02-02 21:16:18 +0100

    Add stub calendar events

---
+++ my-first-project/src/app/stubevents.ts
@@ -0,0 +1,9 @@
+import { CalendarEvent } from "./calendar-event";
+
+export const STUBEVENTS: CalendarEvent[] = [
+    {title:"One"},
+    {title:"Two"},
+    {title:"Three"},
+    {title:"Four"},
+    {title:"Five"},
+]
